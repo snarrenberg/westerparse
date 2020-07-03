5198c0f207c8322b06128aba0d85c1bd920641d3
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-07-03 18:53:58 -0500

    updated sys.path

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./../WesterParse'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.insert(0, os.path.abspath('./../WesterParse'))
 
 # -- Project information -----------------------------------------------------
 
