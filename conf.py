47221fffc37f6ad5f6b32096128936890f14d41b
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-07-03 13:59:44 -0500

    update master doc to index

--- conf.py
+++ conf.py
@@ -24,6 +24,8 @@ version = release =  '2020.1.0-alpha'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
