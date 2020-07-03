5f0117e493ab603e0e3cfde7f277e27e70408ee7
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-07-03 18:45:18 -0500

    created setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="westerparse-pkg-snarrenberg", # Replace with your own username
+    version="1.0.1-alpha",
+    author="Robert Snarrenberg",
+    author_email="snarrenberg@wustl.edu",
+    description="A package for evaluating Westergaardian species counterpoint",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/snarrenberg/WesterParse",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
