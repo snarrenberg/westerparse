6a22dc5bfe61ed3eb8f7a3320e86c6860241a262
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-07-03 18:25:17 -0500

    add init

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# Name:         __init__.py
+# Purpose:      An application for evaluating Westergaardian species counterpoint
+#
+# Author:       Robert Snarrenberg
+# Copyright:    2020
+#-------------------------------------------------------------------------------
+
