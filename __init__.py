665b7765e12a0402dfc068f055541041c54c6e22
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-07-05 20:00:41 -0500

    update initpy

--- __init__.py
+++ __init__.py
@@ -1,29 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['consecutions',
-           'context',
-           'csd',
-           'dependency',
-           'keyFinder',
-           'parser',
-           'rule',
-           'theoryAnalyzerWP',
-           'theoryResultWP',
-           'utilities',
-           'vlChecker',
-           'westerparse']
-
-from westerparse.westerparse import consecutions
-from westerparse.westerparse import context
-from westerparse.westerparse import csd
-from westerparse.westerparse import keyFinder
-from westerparse.westerparse import parser
-from westerparse.westerparse import rule
-from westerparse.westerparse import theoryAnalyzerWP
-from westerparse.westerparse import theoryResultWP
-from westerparse.westerparse import utilities
-from westerparse.westerparse import vlChecker
-from westerparse.westerparse import westerparse
-
 # -----------------------------------------------------------------------------
 # eof
