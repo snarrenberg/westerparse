97c61f91c1217a1b8c2492ba7642db78a4e70028
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-07-07 14:18:37 -0500

    upd setup

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'), 
     python_requires='>=3.5, <4',
-    install_requires['music21'],
+    install_requires=['music21'],
     #dependency_links['https://github.com/cuthbertLab/music21']
 )
