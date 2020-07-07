2090f36763d540297f87b8adac86756fa4167df4
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-07-07 13:43:54 -0500

    upd setup

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="westerparse-pkg-snarrenberg", # Replace with your own username
-    version="1.0.1-alpha",
-    author="Robert Snarrenberg",
-    author_email="snarrenberg@wustl.edu",
-    description="An application for evaluating Westergaardian species counterpoint",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/snarrenberg/WesterParse",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-        'music21',
-        ],
-    dependency_links=[
-        'https://github.com/cuthbertLab/music21']
-)
+setup()
