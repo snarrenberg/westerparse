35533f758f119eaaadff891c652bbb927576735c
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-09-11 10:24:49 -0500

    changed version to 1.1.0alpha

---
+++ westerparse/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# __all__ = ['consecutions',
+#            'context',
+#            'csd',
+#            'dependency',
+#            'keyFinder',
+#            'parser',
+#            'rule',
+#            'theoryAnalyzerWP',
+#            'theoryResultWP',
+#            'utilities',
+#            'vlChecker',
+#            'westerparse']
+# 
+# from . import consecutions
+# from . import context
+# from . import csd
+# from . import keyFinder
+# from . import parser
+# from . import rule
+# from . import theoryAnalyzerWP
+# from . import theoryResultWP
+# from . import utilities
+# from . import vlChecker
+# from . import westerparse
+
+# -----------------------------------------------------------------------------
+# eof
