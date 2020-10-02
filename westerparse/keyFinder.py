2e3c4edb16879ce27e31fb9b901cbec0e29c59f6
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2020-10-02 11:52:13 -0500

    began development of methods for parsing harmonic species: removed leaps test for harmonic species

--- westerparse/keyFinder.py
+++ westerparse/keyFinder.py
@@ -85,7 +85,7 @@ class KeyFinderError(Exception):
 # -----------------------------------------------------------------------------
 
 
-def testKey(score, knote=None, kmode=None):
+def testKey(score, knote=None, kmode=None, kharm=False):
     """Validate and test a key provided by the user."""
     # (1) Validate the user selected key.
     try:
@@ -95,7 +95,7 @@ def testKey(score, knote=None, kmode=None):
         return False
     # (2) Test each part for generic errors.
     try:
-        testValidatedKey(score, knote, kmode)
+        testValidatedKey(score, knote, kmode, kharm)
     except KeyFinderError as kfe:
         kfe.logerror()
         return False
@@ -159,7 +159,7 @@ def validateKeySelection(knote, kmode):
         return userKey
 
 
-def testValidatedKey(score, keynote, mode):
+def testValidatedKey(score, keynote, mode, kharm):
     # Test whether the user-selected key is fits the context.
     userKeyErrors = ''
     partErrorStr = ''
@@ -178,14 +178,14 @@ def testValidatedKey(score, keynote, mode):
         thisTriad = [thisScale.pitchFromDegree(1).name,
                      thisScale.pitchFromDegree(3).name,
                      thisScale.pitchFromDegree(5).name]
-        # Test first and last notes.
+        # (1) Test first and last notes.
         if part.flat.notes[0].pitch.name not in thisTriad:
             error = ('The first note is not a triad pitch.')
             partErrors = partErrors + error + '\n\t'
         if part.flat.notes[-1].pitch.name not in thisTriad:
             error = ('The last note is not a triad pitch.')
             partErrors = partErrors + error + '\n\t'
-        # Test for scale pitches.
+        # (2) Test for scale pitches.
         nonscalars = 0
         for n in part.flat.notes:
             if n.pitch.name not in thisCollection:
@@ -197,12 +197,13 @@ def testValidatedKey(score, keynote, mode):
             error = (str(nonscalars)
                      + ' notes in the line do not belong to the scale.')
             partErrors = partErrors + error + '\n\t'
-        # Test leaps.
+        # (3) Test leaps in monotriadic first, second and fourth species.
         leapPairs = {(note.pitch.name, note.next().pitch.name)
                      for note in part.flat.notes
                      if note.consecutions.rightType == 'skip'}
-        if (part.species in ['first', 'second', 'fourth'] and
-           leapTestWeak(leapPairs, thisTriad) is False):
+        if (not kharm
+                and part.species in ['first', 'second', 'fourth'] and
+                leapTestWeak(leapPairs, thisTriad) is False):
             error = ('At least one leap fails to include a triad pitch.')
             partErrors = partErrors + error + '\n\t'
         if partErrors:
