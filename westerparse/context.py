b076c6de5860e261ef58041ad6619001374f346d
Robert Snarrenberg <snarrenberg@wustl.edu>
2025-04-19 16:35:35 -0500

    worked on validatePars

--- westerparse/context.py
+++ westerparse/context.py
@@ -565,8 +565,10 @@ def validateParts(score, partSelection):
                         initial_pitch = True
                         continue
                     if n.isRest and initial_pitch:
-                        error = ('The first measure has a rest after a note or is incomplete.\nPlease revise the exercise and try again.')
+                        error = ('The first measure has a rest after a note.\nPlease revise the exercise and try again.')
                         raise ContextError(error)
+                    else:
+                        continue
 
         final_measure = part.measure(-1)
         final_measure_notes = final_measure.notes
