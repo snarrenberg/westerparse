7870d53818262a850f30533fc0f1b3d3e3b34d66
Robert Snarrenberg <snarrenberg@wustl.edu>
2025-04-22 15:43:07 -0500

    fixed another problem with formatting parser errors in report

--- westerparse/westerparse.py
+++ westerparse/westerparse.py
@@ -840,9 +840,14 @@ def createParseReport(cxt, generability, partsForParsing, partSelection,
             except KeyError:
                 pass
             else:
-                for errs in cxt.errorsDict[part.name]['parser errors']:
-                   for err in errs: # print(err[0])
+                if isinstance(cxt.errorsDict[part.name]['parser errors'][0], list):
+                    for errs in cxt.errorsDict[part.name]['parser errors']:
+                        for err in errs:
+                            error += '\n\t\t' + str(err)
+                else:
+                    for err in cxt.errorsDict[part.name]['parser errors']:
                         error += '\n\t\t' + str(err)
+
                 # 2025-04-18 turn off unspecified error msg
                 # if not cxt.errorsDict[part.name]['parser errors']:
                 #     error = error + '\n\t\tUnspecified error.'
@@ -851,8 +856,12 @@ def createParseReport(cxt, generability, partsForParsing, partSelection,
             except KeyError:
                 pass
             else:
-                for errs in cxt.errorsDict[part.name]['parser errors']:
-                   for err in errs: # print(err[0])
+                if isinstance(cxt.errorsDict[part.name]['parser errors'][0], list):
+                    for errs in cxt.errorsDict[part.name]['parser errors']:
+                        for err in errs:
+                            error += '\n\t\t' + str(err)
+                else:
+                    for err in cxt.errorsDict[part.name]['parser errors']:
                         error += '\n\t\t' + str(err)
             try:
                 cxt.errorsDict[part.name]['bass']
