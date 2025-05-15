f2ca7dbf4aa80839c52ec539ba2a130de6b565ab
Robert Snarrenberg <snarrenberg@wustl.edu>
2025-05-15 14:41:57 -0500

    replaced tabs with nonbreaking spaces

--- westerparse/utilities.py
+++ westerparse/utilities.py
@@ -59,8 +59,7 @@ def create_html_report(input):
     """
     output = ''
     lines = input.splitlines()
-    for line in lines:
-        line = line.replace('\t', '&nbsp;')
+    lines = [lines.replace('\t', '&nbsp;') for line in lines]
     html_lines = [f"""<p>{line}</p>""" for line in lines]
     return """\n""".join(html_lines)
 
