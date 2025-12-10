85ed607368bf5f28b14006bc165814a2ef1f94df
Robert Snarrenberg <43098819+snarrenberg@users.noreply.github.com>
2025-12-10 11:04:28 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='westerparse',
-    version='2.1.3',
+    version='2.5.3',
     description='An application for evaluating Westergaardian species counterpoint',
     long_description=long_description,
     long_description_content_type='text/markdown', 
